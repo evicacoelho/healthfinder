3fbb7df2425ab6cf236f2e2c3e4ef3c87e9788b8
evicacoelho <evicacoelho@gmail.com>
2024-11-10 15:09:50 -0300

    :note: First commit, starting backend.

---
+++ backend/src/test/java/br/com/healthfinder/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.healthfinder.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
